3a5ce0192eb189c2b2dd03ee9327797ad2d159d5
student <faithnyamburawamui@gmail.com>
2024-06-17 22:35:54 +0300

    made changes

--- web/schoolproject/school/course/models.py
+++ web/schoolproject/school/course/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Courses(models.Model):
+    courses_name=models.CharField(max_length=20)
+    courses_code=models.IntegerField()
+    courses_instuctor=models.CharField(max_length=20)
+    courses_assignment=models.CharField(max_length=20)
+    courses_level=models.CharField(max_length=20)
+    courses_department=models.CharField(max_length=20)
+    courses_syllabus=models.CharField(max_length=20)
+    courses_exams=models.CharField(max_length=20)
+    courses_duration=models.IntegerField()
+
+
+def __str__(self):
+    return f'{self.courses_assignment} {self.courses_department}'
--- web/schoolproject/school/teacher/models.py
+++ web/schoolproject/school/teacher/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    teacher_age=models.IntegerField()
+    teacher_name=models.CharField(max_length=20)
+    teacher_id=models.IntegerField()
+    teacher_course=models.CharField(max_length=20)
+    teacher_class=models.CharField(max_length=20)
+    teacher_description=models.CharField(max_length=20)
+    teacher_occupation=models.CharField(max_length=20)
+    teacher_salary=models.IntegerField()
+    teacher_hobby=models.CharField(max_length=20)
+    teacher_gender=models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.teacher_description} {teacher_age}'
